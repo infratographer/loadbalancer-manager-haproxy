524521948489de40c29e425e0e66a833f051b49f
Matt Siwiec <rizzza@users.noreply.github.com>
2023-05-23 04:28:04 +0000

    lbapi graph client
    Signed-off-by: Matt Siwiec <rizzza@users.noreply.github.com>

--- pkg/lbapi/client.go
+++ pkg/lbapi/client.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 
 	"github.com/shurcooL/graphql"
+	"go.infratographer.com/x/gidx"
 )
+
+// GQLClient is an interface for a graphql client
+type GQLClient interface {
+	Query(ctx context.Context, q interface{}, variables map[string]interface{}) error
+}
 
 // Client creates a new lb api client against a specific endpoint
 type Client struct {
-	client *graphql.Client
+	client GQLClient
 }
 
 // NewClient creates a new lb api client
@@ -21,6 +27,11 @@ func NewClient(url string) *Client {
 
 // GetLoadBalancer returns a load balancer by id
 func (c *Client) GetLoadBalancer(ctx context.Context, id string) (*GetLoadBalancer, error) {
+	_, err := gidx.Parse(id)
+	if err != nil {
+		return nil, err
+	}
+
 	vars := map[string]interface{}{
 		"id": id,
 	}
--- pkg/lbapi/types.go
+++ pkg/lbapi/types.go
@@ -1,30 +1,79 @@
 package lbapi
 
-type GetLoadBalancer struct {
-	LoadBalancer struct {
-		ID    string
-		Name  string
-		Ports struct {
-			Edges []struct {
-				Node struct {
-					Name   string
-					Number int64
-					Pools  []struct {
-						Name     string
-						Protocol string
-						Origins  struct {
-							Edges []struct {
-								Node struct {
-									Name       string
-									Target     string
-									PortNumber int64
-									Active     bool
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	} `graphql:"loadBalancer(id: $id)"`
+type OriginNode struct {
+	ID         string
+	Name       string
+	Target     string
+	PortNumber int64
+	Active     bool
 }
+
+type OriginEdges struct {
+	Node OriginNode
+}
+
+type Origins struct {
+	Edges []OriginEdges
+}
+
+type Pool struct {
+	ID       string
+	Name     string
+	Protocol string
+	Origins  Origins
+}
+
+type PortNode struct {
+	ID     string
+	Name   string
+	Number int64
+	Pools  []Pool
+}
+
+type PortEdges struct {
+	Node PortNode
+}
+
+type Ports struct {
+	Edges []PortEdges
+}
+
+type LoadBalancer struct {
+	ID    string
+	Name  string
+	Ports Ports
+}
+
+type GetLoadBalancer struct {
+	LoadBalancer LoadBalancer `graphql:"loadBalancer(id: $id)"`
+}
+
+// Readable version of the above:
+// type GetLoadBalancer struct {
+// 	LoadBalancer struct {
+// 		ID    string
+// 		Name  string
+// 		Ports struct {
+// 			Edges []struct {
+// 				Node struct {
+// 					Name   string
+// 					Number int64
+// 					Pools  []struct {
+// 						Name     string
+// 						Protocol string
+// 						Origins  struct {
+// 							Edges []struct {
+// 								Node struct {
+// 									Name       string
+// 									Target     string
+// 									PortNumber int64
+// 									Active     bool
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	} `graphql:"loadBalancer(id: $id)"`
+// }
